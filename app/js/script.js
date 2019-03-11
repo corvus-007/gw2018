787f6dc8643c853456ffbdaa0d090fabad70d938
Denis <corvus007@yandex.ru>
2019-03-11 17:33:42 +0300

    Создает промо-слайдер на главной странице

--- app/js/script.js
+++ app/js/script.js
@@ -43,6 +43,60 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // Promo slider
+  var promoSlider = document.querySelector('[data-promo-slider]');
+
+  if (promoSlider) {
+    var countPromoSlides = $('.promo-slide__picture', promoSlider).length;
+    var isHasPromoSliderControls = countPromoSlides > 1 ? true : false;
+
+    $(promoSlider).flickity({
+      setGallerySize: false,
+      // adaptiveHeight: false,
+      wrapAround: true,
+      contain: true,
+      // autoPlay: 3000,
+      pauseAutoPlayOnHover: false,
+      selectedAttraction: 0.018,
+      prevNextButtons: isHasPromoSliderControls,
+      pageDots: isHasPromoSliderControls,
+      // lazyLoad: 1
+    });
+  }
+
+  // Features2 slider
+  var features2Slider = document.querySelector('[data-features2-slider]');
+
+  if (features2Slider) {
+    var $features2Slider = $(features2Slider).flickity({
+      setGallerySize: false,
+      prevNextButtons: false,
+      pageDots: false,
+      draggable: false,
+      bgLazyLoad: 2
+    });
+
+    $('.features2-accordion').accordionjs({
+      beforeOpenSection: function (item) {
+        var index = item.index();
+        $features2Slider.flickity('select', index, false, false);
+      }
+    });
+  }
+
+  var menuItems = document.querySelectorAll('#menu-glavnoe-menyu .current-menu-item');
+
+  if (menuItems) {
+    menuItems.forEach(function (item) {
+      var link = item.querySelector('a');
+      var hrefAttr = link.getAttribute('href');
+      if (/#/.test(hrefAttr)) {
+        item.classList.remove('current-menu-item');
+      }
+    });
+  }
+
+
 
   if (document.querySelector('.flat-filters')) {
     var locationSearch = location.search;
