827a6357a972ecf31179baecbe835981f4566bbe
Denis <corvus007@yandex.ru>
2019-06-06 11:21:09 +0300

    Обновляет additional-script.js

--- app/js/additional-script.js
+++ app/js/additional-script.js
@@ -1,47 +1,56 @@
-if ($().slick) {
-  $('.slider__slides').slick({
-    arrows: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: false,
-    pauseOnHover: false,
-    lazyLoad: 'ondemand',
-    dots: false,
-    speed: 150,
+$(function () {
+  $('#menu-glavnoe-menyu-1 .menu-item').on('click', function () {
+    $('.out-cover--opened').removeClass('out-cover--opened');
+    $('body').removeClass('no-scroll');
+    $('.main-header__toggle').removeClass('out-cover-toggle--fired');
+    $('body').removeClass('is-out-cover-opened');
   });
-}
 
-$('.slider__button').click(function () {
-  $('body').addClass('-popup');
-  $('#getPricePopup').addClass('-visible');
-});
+  if ($().slick) {
+    $('.slider__slides').slick({
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: false,
+      pauseOnHover: false,
+      lazyLoad: 'ondemand',
+      dots: false,
+      speed: 150,
+    });
+  }
 
-$('.main-header__button').click(function () {
-  $('body').addClass('-popup');
-  $('#orderPopup').addClass('-visible');
-});
+  $('.slider__button').click(function () {
+    $('body').addClass('-popup');
+    $('#getPricePopup').addClass('-visible');
+  });
 
-$('.section-intro__to-more').click(function () {
-  $('body').addClass('-popup');
-  $('#aboutPopup').addClass('-visible');
-});
+  $('.main-header__button').click(function () {
+    $('body').addClass('-popup');
+    $('#orderPopup').addClass('-visible');
+  });
 
-$('.popupBlock__close').click(function () {
-  $('body').removeClass('-popup');
-  $('.popupBlock').removeClass('-visible');
-});
+  $('.section-intro__to-more').click(function () {
+    $('body').addClass('-popup');
+    $('#aboutPopup').addClass('-visible');
+  });
 
-$(document).keyup(function (e) {
-  if (e.keyCode == 27) {
+  $('.popupBlock__close').click(function () {
     $('body').removeClass('-popup');
     $('.popupBlock').removeClass('-visible');
-  }
-});
+  });
 
-$('.popupBlock').on('click', function (e) {
-  if (e.target == this) {
-    $('body').removeClass('-popup');
-    $('.popupBlock').removeClass('-visible');
-  }
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      $('body').removeClass('-popup');
+      $('.popupBlock').removeClass('-visible');
+    }
+  });
+
+  $('.popupBlock').on('click', function (e) {
+    if (e.target == this) {
+      $('body').removeClass('-popup');
+      $('.popupBlock').removeClass('-visible');
+    }
+  });
 });
